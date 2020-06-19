5375c2656886c409f8159a25c3be1fcba584ed58
Rsja12 <asocarras10@gmail.com>
2020-06-19 16:40:12 -0400

    run migration and seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'John', email: 'userOne@email.com', password: 'password')
+john = User.create(name: 'John', email: 'userOne@email.com', password: 'password')
+chris = User.create(name: 'Chris', email: 'userTwo@email.com', password: 'password')
+jane = User.create(name: 'Jane', email: 'userThree@email.com', password: 'password')
+
+john.secrets.create(content: 'I want to finish this course')
+john.secrets.create(content: 'I lie way too much for no reason')
+chris.secrets.create(content: 'I love the moana soundtrack')
+chris.secrets.create(content: 'I want a new car')
+jane.secrets.create(content: 'I can eat a gallon of ice cream in one sitting')
+jane.secrets.create(content: 'I cannot function without coffee')
