061734cb9abd99abc8620bb44a0e792251eb5b92
Rsja12 <asocarras10@gmail.com>
2020-06-19 18:45:08 -0400

    add sessions controller to login and logout users

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by(email: params[:user][:email])
+
+        if @user && @user.authenticate(params[:user][:password])
+            render json: @user
+        else
+            render json: { error: 'Invalid Credentials' }, status: :unauthorized
+        end
+    end
+
+    def delete
+    end
+
+end
